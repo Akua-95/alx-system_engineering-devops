0a0dba44f45c7ab720390ab4ce3d8eb61caa8e80
Akua-95 <serwaa.precious@gmail.com>
2024-02-19 16:44:46 -0800

    Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+'''
+Script that displays an employee completed TODO tasks
+'''
+
+
+if __name__ == "__main__":
+
+    """import modules"""
+    import requests
+    import sys
+
+    """send request"""
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
---
+++ 0x15-api/README.md
@@ -0,0 +1,2 @@
+0x15. API
+This project is a part of our back-end development studies, focusing on building APIs using Python and scripting techniques.
